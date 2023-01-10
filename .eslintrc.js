69ff2ab3bdc644f5509d498f47f75e239b47f8e8
lakardion <lakardion@protonmail.com>
2023-01-10 12:49:18 -0300

    fix: eslint issues after adding typescript. - Remove weird errors in IDE bc of babel-eslint not being able to parse TS properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: '@typescript-eslint/parser',
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 13,
   },
+  plugins: ['typescript-eslint'],
+  root: true,
   rules: {},
 }
