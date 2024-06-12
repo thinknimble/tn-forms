48091046412b439254550852724ef173800ca4df
Pari Baker <pari@thinknimble.com>
2024-06-12 11:13:48 -0700

    added new version with data validator

--- lib/esm/types/index.d.ts
+++ lib/esm/types/index.d.ts
@@ -1,65 +1,7 @@
-import Form, { FormField, FormArray } from './forms'
-import Validator, {
-  RequiredValidator,
-  MinLengthValidator,
-  MustMatchValidator,
-  EmailValidator,
-  MinDateValidator,
-  MaxDateValidator,
-  MinimumValueValidator,
-  MaximumValueValidator,
-  PatternValidator,
-  UrlValidator,
-  FormLevelValidator,
-} from './validators'
-import {
-  IDynamicFormValidators,
-  IValidator,
-  TFormInstance,
-  IFormArray,
-  IForm,
-  IFormFieldError,
-  IFormFieldKwargs,
-  IFormArrayKwargs,
-  IFormField,
-  IFormInstance,
-  TFormInstanceFields,
-  FormValue,
-  OptionalFormArgs,
-} from './interfaces'
-import { isNumber, isNumberOrFloat, notNullOrUndefined, fieldGetter } from './utils'
-export default Form
-export {
-  FormField,
-  FormArray,
-  Validator,
-  FormLevelValidator,
-  IDynamicFormValidators,
-  IValidator,
-  TFormInstance,
-  IFormArray,
-  IForm,
-  IFormFieldError,
-  IFormFieldKwargs,
-  IFormArrayKwargs,
-  IFormField,
-  IFormInstance,
-  TFormInstanceFields,
-  RequiredValidator,
-  MinLengthValidator,
-  MustMatchValidator,
-  EmailValidator,
-  MinDateValidator,
-  MaxDateValidator,
-  MinimumValueValidator,
-  MaximumValueValidator,
-  PatternValidator,
-  UrlValidator,
-  FormValue,
-  OptionalFormArgs,
-  isNumber,
-  isNumberOrFloat,
-  notNullOrUndefined,
-  fieldGetter,
-}
+import Form, { FormField, FormArray } from './forms';
+import Validator, { RequiredValidator, MinLengthValidator, MustMatchValidator, EmailValidator, MinDateValidator, MaxDateValidator, MinimumValueValidator, MaximumValueValidator, PatternValidator, UrlValidator, FormLevelValidator } from './validators';
+import { IDynamicFormValidators, IValidator, TFormInstance, IFormArray, IForm, IFormFieldError, IFormFieldKwargs, IFormArrayKwargs, IFormField, IFormInstance, TFormInstanceFields, FormValue, OptionalFormArgs } from './interfaces';
+import { isNumber, isNumberOrFloat, notNullOrUndefined, fieldGetter } from './utils';
+export default Form;
+export { FormField, FormArray, Validator, FormLevelValidator, IDynamicFormValidators, IValidator, TFormInstance, IFormArray, IForm, IFormFieldError, IFormFieldKwargs, IFormArrayKwargs, IFormField, IFormInstance, TFormInstanceFields, RequiredValidator, MinLengthValidator, MustMatchValidator, EmailValidator, MinDateValidator, MaxDateValidator, MinimumValueValidator, MaximumValueValidator, PatternValidator, UrlValidator, FormValue, OptionalFormArgs, isNumber, isNumberOrFloat, notNullOrUndefined, fieldGetter, };
 //# sourceMappingURL=index.d.ts.map
