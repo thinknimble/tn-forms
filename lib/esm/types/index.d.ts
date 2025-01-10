d2f7ee6cdbf07904395f7c25fe312356d1cef165
Damian <lakardion@protonmail.com>
2025-01-10 18:39:13 +0100

    chore: rm /lib

--- lib/esm/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-export default Form;
-import Form from './forms';
-export * from './validators';
-export * from './interfaces';
-export * from './forms';
-export * from './utils';
-//# sourceMappingURL=index.d.ts.map
